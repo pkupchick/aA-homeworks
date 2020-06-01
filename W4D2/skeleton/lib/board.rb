a0fe37bcfe6ef856326f44440cb38b10b07acc52
Peter Kupchick <pkupchick@gmail.com>
2020-06-01 18:47:43 -0400

    Working on the make_move method

--- W4D2/skeleton/lib/board.rb
+++ W4D2/skeleton/lib/board.rb
@@ -2,16 +2,36 @@ class Board
   attr_accessor :cups
 
   def initialize(name1, name2)
+    @cups = Array.new(14) { [] }
+    place_stones
   end
 
   def place_stones
     # helper method to #initialize every non-store cup with four stones each
+    4.times do
+      (0..13).each do |i|
+        unless i == 6 || i == 13
+          @cups[i] << :stone
+        end
+      end
+    end
+    @cups
   end
 
   def valid_move?(start_pos)
+    raise "Invalid starting cup" if start_pos < 0 || start_pos > 12
+    raise "Starting cup is empty" if @cups[start_pos].empty?
   end
 
   def make_move(start_pos, current_player_name)
+    stones = @cup[start_pos]
+    @cup[start_pos] = []
+
+    start_idx = start_pos + 1
+    until stones.empty?
+
+    end
+
   end
 
   def next_turn(ending_cup_idx)
@@ -32,3 +52,9 @@ class Board
   def winner
   end
 end
+
+# test = Board.new(:john, :sam)
+
+# p test.place_stones
+
+# p test.cups
